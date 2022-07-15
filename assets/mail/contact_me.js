2b01035dcf1aaca5f96850b09070f12a5fe3229d
Nabeel Moosajee <58814753+nabeelmoosajee@users.noreply.github.com>
2022-07-15 22:39:19 +0545

    added api link and made changes to index

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://zhpu84gpnf.execute-api.us-west-2.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
